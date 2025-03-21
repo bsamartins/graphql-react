b6facf80d1e5becaff7ae1e7fbca28327807ea93
Bernardo Martins <bernardo.martins@utmost.co>
2025-03-21 22:08:41 +0000

    Refactor pagination

--- api/src/main/kotlin/io/bsamartins/sandbox/graphql/data/MovieService.kt
+++ api/src/main/kotlin/io/bsamartins/sandbox/graphql/data/MovieService.kt
@@ -2,7 +2,6 @@ package io.bsamartins.sandbox.graphql.data
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.bsamartins.sandbox.graphql.codegen.types.Movie
-import io.bsamartins.sandbox.graphql.graphql.PageRequest
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
 import java.util.*
