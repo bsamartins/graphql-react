45377a117c7914ac2b75de73fb9915c4d5bc7457
Bernardo Martins <bernardo.martins@utmost.co>
2025-03-21 23:59:44 +0000

    Add actor data fetcher

--- api/src/main/kotlin/io/bsamartins/sandbox/graphql/graphql/ActorsDataFetcher.kt
+++ api/src/main/kotlin/io/bsamartins/sandbox/graphql/graphql/ActorsDataFetcher.kt
@@ -2,13 +2,18 @@ package io.bsamartins.sandbox.graphql.graphql
 
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsData
+import com.netflix.graphql.dgs.DgsDataLoader
 import com.netflix.graphql.dgs.DgsQuery
 import graphql.relay.Connection
 import graphql.schema.DataFetchingEnvironment
 import io.bsamartins.sandbox.graphql.codegen.types.Actor
 import io.bsamartins.sandbox.graphql.codegen.types.Movie
 import io.bsamartins.sandbox.graphql.data.ActorService
+import org.dataloader.MappedBatchLoader
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
 import io.bsamartins.sandbox.graphql.data.Actor as ActorData
+
 
 @DgsComponent
 class ActorsDataFetcher(
@@ -20,10 +25,24 @@ class ActorsDataFetcher(
         .asConnection(env)
 
     @DgsData(parentType = "Movie", field = "actors")
-    fun movieActors(env: DataFetchingEnvironment): List<Actor> {
+    fun movieActors(env: DataFetchingEnvironment): CompletableFuture<List<Actor>> {
+        val dataLoader = env.getDataLoader<String, Actor>("actors")!!
         val movie = env.getSource<Movie>()!!
-        return movie.actors.mapNotNull { actorService.findByName(it.name) }
-            .map { it.toModel() }
+        return dataLoader.loadMany(movie.actors.map { it.name })
+            .thenApply { it.filterNotNull() }
+    }
+}
+
+@DgsDataLoader(name = "actors")
+class DirectorsDataLoader(
+    var actorService: ActorService,
+) : MappedBatchLoader<String, Actor> {
+
+    override fun load(keys: Set<String>): CompletionStage<Map<String, Actor>> {
+        return CompletableFuture.supplyAsync {
+            keys.mapNotNull { key -> actorService.findByName(key)?.let { key to it.toModel() } }
+                .toMap()
+        }
     }
 }
 
