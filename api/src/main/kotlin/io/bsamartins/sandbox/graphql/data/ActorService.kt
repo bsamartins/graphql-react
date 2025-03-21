239f6e16237bf0d8a5b9feef2306efba978133a6
Bernardo Martins <bernardo.martins@utmost.co>
2025-03-21 19:05:24 +0000

    Refactor data services

--- api/src/main/kotlin/io/bsamartins/sandbox/graphql/data/ActorService.kt
+++ api/src/main/kotlin/io/bsamartins/sandbox/graphql/data/ActorService.kt
@@ -4,17 +4,21 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.netflix.dgs.codegen.generated.types.Actor
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class ActorService(
     objectMapper: ObjectMapper,
 ) {
-    private val actors: List<Actor> by lazy {
+    private val actors: Map<String, Actor> by lazy {
         val arrayNode = objectMapper.readTree(ClassPathResource("data/actors.json").file)
-        arrayNode.map { node ->
-            Actor(id = node.get("objectID").asText(), name = node.get("name").asText())
+        val result = TreeMap<String, Actor>()
+        arrayNode.forEach { node ->
+            val actor = Actor(id = node.get("objectID").asText(), name = node.get("name").asText())
+            result[actor.id] = actor
         }
+        result
     }
 
-    fun listAll(): List<Actor> = actors
+    fun listAll(): List<Actor> = actors.values.toList()
 }
--- api/src/main/kotlin/io/bsamartins/sandbox/graphql/data/MovieService.kt
+++ api/src/main/kotlin/io/bsamartins/sandbox/graphql/data/MovieService.kt
@@ -1,21 +1,24 @@
 package io.bsamartins.sandbox.graphql.data
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.netflix.dgs.codegen.generated.types.Actor
 import com.netflix.dgs.codegen.generated.types.Movie
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class MovieService(
     objectMapper: ObjectMapper,
 ) {
-    private val movies: List<Movie> by lazy {
+    private val movies: Map<String, Movie> by lazy {
         val arrayNode = objectMapper.readTree(ClassPathResource("data/movies.json").file)
-        arrayNode.map { node ->
-            Movie(id = node.get("objectID").asText(), name = node.get("title").asText())
+        val result = TreeMap<String, Movie>()
+        arrayNode.forEach { node ->
+            val movie = Movie(id = node.get("objectID").asText(), name = node.get("title").asText())
+            result[movie.id] = movie
         }
+        result
     }
 
-    fun listAll(): List<Movie> = movies
+    fun listAll(): List<Movie> = movies.values.toList()
 }
