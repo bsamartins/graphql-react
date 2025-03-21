be477f15a872ae6a995507c4ff29c94648f8ad59
Bernardo Martins <bernardo.martins@utmost.co>
2025-03-21 14:02:55 +0000

    Setup proxy

--- ui/src/Movies.tsx
+++ ui/src/Movies.tsx
@@ -2,18 +2,22 @@ import {useQuery} from "@apollo/client";
 import {LIST_MOVIES} from "./gql";
 
 export default function Movies() {
-    const { loading, error, data } = useQuery<Movie[]>(LIST_MOVIES);
+    const { loading, error, data } = useQuery<ListMovies>(LIST_MOVIES);
 
     if (loading) return <>'Loading...'</>;
     if (error) return <>`Error! ${error.message}`</>;
 
     return (
         <div>
-            {data?.map((movie) => (
+            {data?.data?.map((movie) => (
                 <div key={movie.id}>{movie.name}</div>
             ))}
         </div>
     );
+}
+
+interface ListMovies {
+    data: Movie[]
 }
 
 interface Movie {
