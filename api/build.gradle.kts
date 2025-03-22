b17ec7cc5f7ac810d61e1754f6d597cede980049
Bernardo Martins <bernardo.martins@utmost.co>
2025-03-22 17:54:20 +0000

    Refactor data loader to actuator endpoint

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
     implementation("com.netflix.graphql.dgs:graphql-dgs-pagination")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     implementation("org.hibernate.search:hibernate-search-mapper-orm:${libs.versions.hibernate.search.get()}")
     implementation("org.hibernate.search:hibernate-search-backend-lucene:${libs.versions.hibernate.search.get()}")
--- api/src/main/kotlin/io/bsamartins/sandbox/graphql/data/DataInitialization.kt
+++ api/src/main/kotlin/io/bsamartins/sandbox/graphql/data/DataInitialization.kt
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.github.oshai.kotlinlogging.KotlinLogging
-import jakarta.annotation.PostConstruct
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint
 import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
+import java.util.concurrent.atomic.AtomicBoolean
 
 @Component
+@WebEndpoint(id = "load-data")
 class DataInitialization(
     private val objectMapper: ObjectMapper,
     private val movieRepository: MovieRepository,
@@ -16,15 +19,29 @@ class DataInitialization(
     private val movieCastRepository: MovieCastRepository,
 ) {
     private val logger = KotlinLogging.logger {}
+    private val atomicBoolean = AtomicBoolean(false)
 
-    @PostConstruct
+    @WriteOperation
     fun populate() {
+        if (atomicBoolean.get()) {
+            atomicBoolean.set(true)
+            try {
+                load()
+            } finally {
+                atomicBoolean.set(false)
+            }
+        } else {
+            logger.info { "Load in progress" }
+        }
+    }
+
+    private fun load() {
         logger.info { "Populating database" }
 
         logger.info { "Loading actors" }
         val actors = objectMapper.readValue<List<ActorData>>(ClassPathResource("data/actors.json").file)
         val actorsByName = actors.associateBy { it.name }
-        actors.forEach { actor ->
+        actors.stream().parallel().forEach { actor ->
             actorRepository.save(
                 Actor(id = actor.id, name = actor.name)
             )
@@ -32,17 +49,22 @@ class DataInitialization(
 
         logger.info { "Loading movies" }
         val movies = objectMapper.readValue<List<MovieData>>(ClassPathResource("data/movies.json").file)
-        movies.forEach { movie ->
-            movieRepository.save(
-                Movie(id = movie.id, title = movie.title)
-            )
-            movie.actors.forEach { actorName ->
-                val actor = actorsByName[actorName]
-                if (actor != null) {
-                    movieCastRepository.save(MovieCast(actorId = actor.id, movieId = movie.id))
-                }
-            }
+        var progress = 0
+        movies.chunked(50).stream().parallel().forEach { chunks ->
+            val toInsert = chunks.map { movie -> Movie(id = movie.id, title = movie.title) }
+            movieRepository.saveAll(toInsert)
+            progress += toInsert.count()
+            logger.info { "Movie inserted $progress/${movies.size}" }
         }
+
+        movies.flatMap { movie ->
+            movie.actors.mapNotNull { actorName ->
+                actorsByName[actorName]?.let { actor -> movie to actor }
+            }
+        }.parallelStream()
+            .forEach { (movie, actor) ->
+                movieCastRepository.save(MovieCast(actor.id, movie.id))
+            }
 
         logger.info { "Done" }
     }
