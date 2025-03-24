cb9ded1a824b3c88bfcfd7bcb8e379f68dbc84d5
Bernardo Martins <bernardo.martins@utmost.co>
2025-03-24 00:04:21 +0000

    Simplify data fetchers

--- api/src/main/kotlin/io/bsamartins/sandbox/graphql/graphql/ActorsDataFetcher.kt
+++ api/src/main/kotlin/io/bsamartins/sandbox/graphql/graphql/ActorsDataFetcher.kt
@@ -7,9 +7,7 @@ import graphql.relay.Connection
 import graphql.schema.DataFetchingEnvironment
 import io.bsamartins.sandbox.graphql.codegen.types.Actor
 import io.bsamartins.sandbox.graphql.codegen.types.Cast
-import io.bsamartins.sandbox.graphql.codegen.types.Movie
 import io.bsamartins.sandbox.graphql.data.ActorService
-import io.bsamartins.sandbox.graphql.data.MovieService
 import io.bsamartins.sandbox.graphql.graphql.dataloaders.ActorDataLoader
 import java.util.concurrent.CompletableFuture
 import io.bsamartins.sandbox.graphql.data.Actor as ActorData
@@ -18,28 +16,17 @@ import io.bsamartins.sandbox.graphql.data.Actor as ActorData
 @DgsComponent
 class ActorsDataFetcher(
     private val actorService: ActorService,
-    private val movieService: MovieService,
 ) {
     @DgsQuery
     fun actors(env: DataFetchingEnvironment): Connection<Actor> = actorService.listAll()
         .map { it.toModel() }
         .asConnection(env)
 
-    @DgsData(parentType = "Movie", field = "cast")
-    fun movieActors(env: DataFetchingEnvironment): CompletableFuture<List<Cast>> {
+    @DgsData(parentType = "Cast", field = "actor")
+    fun castActor(env: DataFetchingEnvironment): CompletableFuture<Actor> {
         val actorDataLoader = ActorDataLoader.get(env)
-        val movie = env.getSource<Movie>()!!
-        val cast = movieService.getCast(movie.id)
-        return actorDataLoader.loadMany(cast.map { it.actorId })
-            .thenApply { actors ->
-                val actorsById = actors.associateBy { it.id }
-                cast.map { casting ->
-                    Cast(
-                        actor = actorsById.getValue(casting.actorId),
-                        character = casting.character,
-                    )
-                }
-            }
+        val cast = env.getSource<Cast>()!!
+        return actorDataLoader.load(cast.actor.id)
     }
 }
 
--- api/src/main/kotlin/io/bsamartins/sandbox/graphql/graphql/MoviesDataFetcher.kt
+++ api/src/main/kotlin/io/bsamartins/sandbox/graphql/graphql/MoviesDataFetcher.kt
@@ -1,10 +1,13 @@
 package io.bsamartins.sandbox.graphql.graphql
 
 import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsData
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
 import graphql.relay.Connection
 import graphql.schema.DataFetchingEnvironment
+import io.bsamartins.sandbox.graphql.codegen.types.Actor
+import io.bsamartins.sandbox.graphql.codegen.types.Cast
 import io.bsamartins.sandbox.graphql.codegen.types.Movie
 import io.bsamartins.sandbox.graphql.data.MovieService
 import io.bsamartins.sandbox.graphql.data.Movie as MovieData
@@ -22,6 +25,18 @@ class MoviesDataFetcher(
         return movieService.search(pageRequest, query = query)
             .map { it.toModel() }
             .asConnection(env)
+    }
+
+    @DgsData(parentType = "Movie", field = "cast")
+    fun movieActors(env: DataFetchingEnvironment): List<Cast> {
+        val movie = env.getSource<Movie>()!!
+        return movieService.getCast(movie.id)
+            .map {
+                Cast(
+                    actor = Actor(id = it.actorId, name = ""),
+                    character = it.character,
+                )
+            }
     }
 }
 
