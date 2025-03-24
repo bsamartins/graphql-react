52af8a39c100811cf3dd4398b52fc0bd4926d6dd
Bernardo Martins <bernardo.martins@utmost.co>
2025-03-24 11:46:17 +0000

    Configure vite

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+// import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     // depending on your application, base can also be "/"
     base: '',
     plugins: [
         react(),
-        viteTsconfigPaths()
+        viteTsconfigPaths(),
+        // tailwindcss()
     ],
     server: {
         // this ensures that the browser opens upon server start
